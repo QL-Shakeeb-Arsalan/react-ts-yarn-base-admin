5f3f3cadb0be1f92334aafe0918058fda52742e8
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2024-10-02 00:22:08 +0530

    [feat]: Logic improvement in nested route mechanism

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -8,21 +8,36 @@ export interface IRoutes {
 
 const sliceOutLastPath = (path: string) => `/${path.split('/').filter(Boolean).slice(0, -1).join('/')}`
 
+function getParentRouteIndex(path: string, routes: IRoutes[]) {
+  const nestedPath = sliceOutLastPath(path)
+  const parentRouteIndex = routes.findIndex((r: IRoutes) => r.path === nestedPath)
+
+  if (parentRouteIndex > -1 && path !== '/') {
+    return parentRouteIndex
+  } else {
+    return getParentRouteIndex(nestedPath, routes)
+  }
+}
+
 export function generateNestedRoutes(routes: IRoutes[]): IRoutes[] {
   for (const route of routes) {
     const { path } = route
-    const nestedPath = sliceOutLastPath(path)
-    const parentRouteIndex = routes.findIndex((r: IRoutes) => r.path === nestedPath)
 
-    if (parentRouteIndex > -1) {
+    if (path !== '/') {
+      const parentRouteIndex = getParentRouteIndex(path, routes)
+      const children = routes[parentRouteIndex].children
+      const parentPath = routes[parentRouteIndex].path
+
+      // Add a children in a route
       routes[parentRouteIndex].children = [
-        ...(routes[parentRouteIndex].children || []),
+        ...(children || []),
         {
           ...route,
-          path: path.substring(path.lastIndexOf('/') + 1),
+          // Remove parent route pathname  /blog/:id => :id
+          path: path.replace(parentPath === '/' ? '/' : `${parentPath}/`, ''),
         },
       ]
-      routes = routes.filter((r: IRoutes) => r.path !== path)
+      routes = routes.filter(r => r.path !== path && path !== '/')
     }
   }
 
@@ -31,7 +46,7 @@ export function generateNestedRoutes(routes: IRoutes[]): IRoutes[] {
 
 export function generateRoutes() {
   const pages = import.meta.glob('../pages/**/*.tsx')
-  const routes = []
+  const routes: IRoutes[] = []
   // eslint-disable-next-line sonarjs/slow-regex
   const catchRegex = /\[(.+)\]/g
 
@@ -46,7 +61,7 @@ export function generateRoutes() {
       routePath = routePath.slice(0, -6) || '/'
     }
 
-    const route = {
+    const route: IRoutes = {
       element: <Component />,
       path: routePath,
     }
@@ -55,7 +70,7 @@ export function generateRoutes() {
   }
 
   // sort routes to ensure that parent routes are procesed before child routes
-  routes.sort((a, b) => a.path.split('/').length - b.path.split('/').length)
+  routes.sort((a, b) => b.path.split('/').length - a.path.split('/').length)
 
   // create a nested structure
   return generateNestedRoutes(routes)
